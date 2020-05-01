9d0918b0f0a5889f84b8c50c2e73ced71175b0bc
Andrew Heiss <andrew@andrewheiss.com>
2020-04-30 23:21:49 -0400

    Add slides

---
+++ R/build.R
@@ -0,0 +1,15 @@
+# Render all the .Rmd files in static/slides
+# blogdown::build_dir("static/slides")
+
+
+# Convert all slide HTML files to PDF
+# slide_names_with_ext <- list.files(here::here("static/slides/"), pattern = "*.html")
+# 
+# slide_names_sans_ext <- tools::file_path_sans_ext(slide_names_with_ext)
+# 
+# pdfize <- function(slide_name) {
+#   pagedown::chrome_print(here::here(paste0("static/slides/", slide_name, ".html")),
+#                          output = here::here(paste0("static/slides/", slide_name, ".pdf")))
+# }
+# 
+# sapply(slide_names_sans_ext, pdfize)
